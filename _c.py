40a75498afaac7c9d4e3b012e8163be6cf613f84
CampusNetAuth <campus@example.com>
2026-06-01 23:00:53 +0800

    fix: 修正学校名称为广州市工贸技师学院

--- _c.py
+++ _c.py
@@ -1,4 +1,5 @@
 import subprocess
 subprocess.run(["git", "add", "-A"], check=True)
-subprocess.run(["git", "commit", "-m", "docs: 重写 README，简洁功能介绍 + 代理 Q&A"], check=True)
-print("Commit done")
+subprocess.run(["git", "commit", "-m", "fix: 修正学校名称为广州市工贸技师学院"], check=True)
+subprocess.run(["git", "push", "origin", "main"], check=True)
+print("Done")
