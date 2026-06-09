661fd30437e130c9cfab65057b7faf6a7a257b4f
CampusNetAuth <campus@example.com>
2026-06-09 20:56:57 +0800

    chore: 清理临时脚本，.gitignore 通配临时文件

--- _c.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-subprocess.run(["git", "add", "-A"], check=True)
-subprocess.run(["git", "commit", "-m", "fix: 修正学校名称为广州市工贸技师学院"], check=True)
-subprocess.run(["git", "push", "origin", "main"], check=True)
-print("Done")
